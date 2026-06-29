e497db8bc298eb4744069fb417bd5099ee3d6f04
Ganesh <ganeshgarje716@gmail.com>
2026-06-29 23:31:48 +0530

    extends JpaRepository

--- Spring-RestAPI-Student/src/main/java/com/ganesh/repository/StudentRepository.java
+++ Spring-RestAPI-Student/src/main/java/com/ganesh/repository/StudentRepository.java
@@ -1,5 +1,9 @@
 package com.ganesh.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ganesh.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 }
