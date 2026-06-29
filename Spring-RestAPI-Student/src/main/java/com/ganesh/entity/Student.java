333b6b730655760bc721205755bbd2a05dd756c0
Ganesh <ganeshgarje716@gmail.com>
2026-06-29 23:38:50 +0530

    Add Annotations @Data @Entity @AllArgsConstructor

--- Spring-RestAPI-Student/src/main/java/com/ganesh/entity/Student.java
+++ Spring-RestAPI-Student/src/main/java/com/ganesh/entity/Student.java
@@ -1,5 +1,12 @@
 package com.ganesh.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
 public class Student {
 
 }
