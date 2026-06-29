a70723f6512516865a538ef04909ae9745a83df4
Ganesh <ganeshgarje716@gmail.com>
2026-06-29 23:13:32 +0530

    Create Spring Rest API Student Project

---
+++ Spring-RestAPI-Student/src/main/java/com/ganesh/SpringRestApiStudentApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiStudentApplication.class, args);
+	}
+
+}
