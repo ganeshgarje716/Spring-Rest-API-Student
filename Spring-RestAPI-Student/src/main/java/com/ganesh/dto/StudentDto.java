f81c821fa7d12dc457db3208d8586504b452e3a7
Ganesh <ganeshgarje716@gmail.com>
2026-06-30 22:56:06 +0530

    Add Variables and Validation

--- Spring-RestAPI-Student/src/main/java/com/ganesh/dto/StudentDto.java
+++ Spring-RestAPI-Student/src/main/java/com/ganesh/dto/StudentDto.java
@@ -1,8 +1,28 @@
 package com.ganesh.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StudentDto {
 
+	
+	
+	@NotNull(message = "Name Is Required")
+	private String name;
+	
+	@NotNull(message = "Address Is Required")
+	private String address;
+	
+	@NotBlank(message = "Age Is Required")
+	private Integer age;
+	
+	@NotNull(message = "Email Is Required")
+	private String email;
+	
+	@NotNull(message = "Mobile No Is Required")
+	private String mobileNo;
+	
+	
 }
